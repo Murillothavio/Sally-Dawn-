b76c2c0d50977b41739780c5ca5bb6c1f054fe02
VictorCAbr <52220641+VictorCAbr@users.noreply.github.com>
2019-09-18 16:51:27 -0300

    1 som

---
+++ Projeto Final Sally Dawn/Assets/Scripts/Ambiente.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ambiente : MonoBehaviour
+{
+    public enum Fase { Neutro, Alegre, Triste, Raiva, Nojo, Medo, Etereo}
+    public Fase ambiente;
+    public AudioClip som;
+    private AudioSource aus;
+    // Start is called before the first frame update
+    void Start()
+    {
+        aus = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        aus.clip = som;
+    }
+}
