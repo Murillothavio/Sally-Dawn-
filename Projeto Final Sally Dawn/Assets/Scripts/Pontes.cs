6be8e6f0cdd57989a9967ea4736fd568ebceb70f
VictorCAbr <victorcdeabrantes@gmail.com>
2020-05-13 01:08:04 -0300

    Ponte p1

---
+++ Projeto Final Sally Dawn/Assets/Scripts/Pontes.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pontes : MonoBehaviour
+{
+    public bool Ativo;
+    public Animator anim;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (anim != null)
+            anim.SetBool("Abrir", !Ativo);
+        else
+            Debug.LogError("sem animator da ponte");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Ativo = true;
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        Ativo = false;
+    }
+}
