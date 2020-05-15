c81fe8965c2ad3688d2753f27f5a2261775fcddb
VictorCAbr <victorcdeabrantes@gmail.com>
2020-05-15 00:16:48 -0300

    morte queda

---
+++ Projeto Final Sally Dawn/Assets/Scripts/Player/MorteQueda.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MorteQueda : MonoBehaviour
+{
+    public Transform Cam;
+    public float Distancia;
+    [Range(1,100)]
+    public float Max;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (Cam == null)
+            Debug.LogError("No cam in Morte queda");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Cam != null)
+            Distancia = GameMaster.CalcularDist(transform, Cam);
+
+        if (Distancia > Max)
+        {
+            GetComponent<AndarPlayer>().Morrer();
+            Debug.Log("morrer");
+        }
+    }
+}
