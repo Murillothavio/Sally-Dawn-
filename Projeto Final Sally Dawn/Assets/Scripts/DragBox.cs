d5bbf4a144846c655c49522387e3b277787e3464
VictorCAbr <victorcdeabrantes@gmail.com>
2020-06-01 23:03:51 -0300

    respawnbox

--- Projeto Final Sally Dawn/Assets/Scripts/DragBox.cs
+++ Projeto Final Sally Dawn/Assets/Scripts/DragBox.cs
@@ -9,17 +9,34 @@ public class DragBox : MonoBehaviour
     public bool seg, encostado;
     [SerializeField]
     private Vector3 DeltaPosi;
+    [Header("_____________")]
+    public bool Draw;
+    [Range(1, 100)]
+    public float MaxRange = 30;
+    private float MinRange = 5;
+    public Vector3 Inicial;
+    public float DistanciaAtual, DistanP;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         play = GameObject.Find("Player");
         seg = play.GetComponent<AndarPlayer>().Segurando;
+        Inicial = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
+        DistanciaAtual = CalcularDistBox(gameObject);
+        if (DistanciaAtual > MaxRange)
+        {
+            DistanP = CalcularDistBox(play);
+            if (!(DistanP < MinRange))
+                transform.position = Inicial;
+        }
+
         seg = play.GetComponent<AndarPlayer>().Segurando;
         if (seg && encostado)
         {
@@ -52,5 +69,30 @@ public class DragBox : MonoBehaviour
     {
         if (other.gameObject == play)
             encostado = false;
+    }
+
+
+    public float CalcularDistBox(GameObject a)
+    {
+        float CatetoX = a.transform.position.x - Inicial.x;
+        float CatetoY = a.transform.position.y - Inicial.y;
+
+        return Mathf.Sqrt(CatetoX * CatetoX + CatetoY * CatetoY);
+    }
+    private void OnDrawGizmos()
+    {
+        if (Draw)
+        {
+            Vector3 ini;
+            if (Inicial == Vector3.zero)
+                ini = transform.position;
+            else
+                ini = Inicial;
+            Gizmos.color = new Color(0, 1, 0, .5f);
+            Gizmos.DrawSphere(ini, MaxRange);
+
+            Gizmos.color = new Color(0, 0, 1, .9f);
+            Gizmos.DrawSphere(ini, MinRange);
+        }
     }
 }
