83423c2be355ef8475632fb39df3c322dcbe8308
VictorCAbr <victorcdeabrantes@gmail.com>
2020-06-16 03:49:04 -0300

    Ressurreicao

---
+++ Projeto Final Sally Dawn/Assets/Scripts/LookGirl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookGirl : MonoBehaviour
+{
+    private Transform Girl;
+    private Vector3 Posi;
+    public float h = 3.5f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Girl = GameMaster.gm.Player.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Posi = Girl.position;
+        Posi.y += h;
+        transform.LookAt(Posi);
+    }
+}
