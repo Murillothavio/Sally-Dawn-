358a687d8c0e8c96e18a162bd3d8a432187f8588
VictorCAbr <victorcdeabrantes@gmail.com>
2020-06-19 08:15:19 -0300

    Rei Poro

---
+++ Projeto Final Sally Dawn/Assets/Scripts/ReiRato.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReiRato : MonoBehaviour
+{
+    public bool Corra, Reset, SetT, Gambiarra;
+    private Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+
+    private void Update()
+    {
+        if (SetT)
+        {
+            SetT = false;
+            if (Corra)
+            {
+                anim.SetTrigger("Reset");
+                Corra = false;
+                Gambiarra = false;
+            }
+            else
+            {
+                Corra = true;
+                anim.SetTrigger("Corra");
+            }
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            SetT = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            if (Gambiarra)
+                SetT = true;
+            else
+                Gambiarra = true;
+        }
+    }
+}
---
+++ Projeto Final Sally Dawn/Assets/Scripts/ReiRatoAni.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReiRatoAni : MonoBehaviour
+{
+    public bool Corra, Reset;
+    private Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        anim.SetBool("Corra", Corra);
+
+        anim.SetBool("Reset", Reset);
+    }
+}
