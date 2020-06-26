36549441731c1b2f78eadcd4953a041dd0b2d75d
Murillothavio <44687313+Murillothavio@users.noreply.github.com>
2020-06-25 22:56:17 -0300

    Caixas explosivas

---
+++ Projeto Final Sally Dawn/Assets/Scripts/CaixaBoom.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaixaBoom : MonoBehaviour
+{
+    public GameObject Boom;
+
+    private void OnDestroy()
+    {
+        GameObject b = Instantiate(Boom, transform.position, Quaternion.identity)as GameObject;
+        Destroy(b, 2);
+    }
+
+}
