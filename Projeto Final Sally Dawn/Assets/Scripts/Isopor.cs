b93f894a786711ca7ede537213f8286266de7d71
VictorCAbr <victorcdeabrantes@gmail.com>
2020-06-25 22:13:15 -0300

    Testa esse odio
    Raiva**

---
+++ Projeto Final Sally Dawn/Assets/Scripts/Isopor.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Isopor : MonoBehaviour
+{
+    public bool Reset,Perto;
+    private Animator animi;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animi = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Reset)
+        {
+            animi.SetTrigger("Reset");
+            Reset = false;
+        }
+        if (Perto)
+        {
+            Perto = false;
+            animi.SetTrigger("Perto");
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+
+            animi.SetTrigger("Perto");
+        }
+    }
+}
+
+
