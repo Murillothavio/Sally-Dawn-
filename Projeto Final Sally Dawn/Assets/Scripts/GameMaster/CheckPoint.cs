10976a0a869f53ed0fce98acedb3871d30677efc
VictorCAbr <victorcdeabrantes@gmail.com>
2020-06-27 08:18:03 -0300

    Girassol um começo

--- Projeto Final Sally Dawn/Assets/Scripts/GameMaster/CheckPoint.cs
+++ Projeto Final Sally Dawn/Assets/Scripts/GameMaster/CheckPoint.cs
@@ -4,16 +4,28 @@ using UnityEngine;
 
 public class CheckPoint : MonoBehaviour
 {
+    public bool Salvo;
+    public float Cor;
+    public MeshRenderer[] mr;
+    public Material[] material;
     // Start is called before the first frame update
     void Start()
     {
-        
+        for (int i = 0; i < transform.GetChild(0).childCount; i++)
+        {
+            mr[i] = transform.GetChild(0).GetChild(i).GetComponent<MeshRenderer>();
+            material[i] = mr[i].material;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Cor = Mathf.MoveTowards(Cor, (Salvo ? 1 : 0), 0.5f);
+        // for (int i = 0; i < material.Length; i++)
+        //{
+        material[0].SetFloat("_TransicaoGirassol", Cor);
+       // }
     }
     private void OnTriggerEnter(Collider other)
     {
